82ed34682de30005bc8670adf49e8c59fc77ffba
본ìbuffpsigit config --global user.email 깃허브 <rlawlstjd778@naver.com>
2023-08-03 15:28:34 +0900

    first setting

---
+++ src/main/java/programmers/Main.java
@@ -0,0 +1,7 @@
+package programmers;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("프로그래머스 고득점 kit");
+    }
+}
---
+++ src/main/java/programmers/highScore/sort/Main.java
@@ -0,0 +1,7 @@
+package programmers.highScore.sort;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(1);
+    }
+}
